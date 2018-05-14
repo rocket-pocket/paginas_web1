e2e1980e0beaadd9627244b1162acb7bf0bd5c86
rocket-pocket <verdudaniel7@gmail.com>
2018-05-14 12:53:53 +0200

    metadatos

--- paginas_web1/paginas_web01/Hola_mundo1/scripts/app.js
+++ paginas_web1/paginas_web01/Hola_mundo1/scripts/app.js
@@ -1,2 +1,5 @@
-let saludo ='Hola pintamonas desde el fichero'
-console.log(saludo)
+//let saludo ='Hola pintamonas desde el fichero'
+//console.log(saludo)
+
+ //alert('buenos dias')
+ //prompt('ingresa tu nombre aquí', 'harry potter')
